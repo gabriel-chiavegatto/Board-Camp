cc7a5d691aeb26aceb65ef64ceb7302be986e28d
Gabriel Chiavegatto <chiavegattogomes@gmail.com>
2022-10-03 10:21:21 -0300

    get game list

--- src/controllers/gameList.js
+++ src/controllers/gameList.js
@@ -1,10 +1,21 @@
 import { connection } from "../database.js";
 
-export default async function gameList(req,res){
-    try{
-        const games = await connection.query("SELECT * FROM games");
-        res.status(200).send(games.rows);
-    } catch(error){
+export default async function gameList(req, res) {
+    try {
+        const {name} = req.query;
+
+        const games = (await (connection.query('SELECT * FROM games'))).rows;
+
+        for (let i = 0; i < games.length; i++) {
+            const categoryName = (await connection.query('SELECT name FROM categories WHERE id = $1',[games[i].categoryId])).rows[0];
+            games[i] = {
+                ...games[i],
+                categoryName: categoryName.name
+            }
+        }
+
+        res.status(200).send(games);
+    } catch (error) {
         console.log(error);
         res.sendStatus(422);
     }
--- src/controllers/registerGame.js
+++ src/controllers/registerGame.js
@@ -19,12 +19,12 @@ export default async function registerGame(req, res) {
 
         const confirmCategoryId = await connection.query('SELECT * FROM categories WHERE id = $1',[categoryId]);
         if(confirmCategoryId.rows.length === 0){
-            return res.sendStatus(400)
+            return res.sendStatus(404)
         }
 
         const confirmNameAvailability = await connection.query('SELECT * FROM games WHERE name = $1',[name]);
         if(confirmNameAvailability.rows.length !== 0){
-            return res.sendStatus(400)
+            return res.sendStatus(409)
         }
 
         await connection.query('INSERT INTO games (name, image, "stockTotal", "categoryId", "pricePerDay") VALUES ($1,$2,$3,$4,$5);',[name, image, stockTotal, categoryId, pricePerDay]);
