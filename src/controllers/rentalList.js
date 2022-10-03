12a8e5e3e130ed8d04ffc66524221d6ba56bbfb9
Gabriel Chiavegatto <chiavegattogomes@gmail.com>
2022-10-03 15:29:40 -0300

    query strings on get rentals

--- src/controllers/rentalList.js
+++ src/controllers/rentalList.js
@@ -2,21 +2,30 @@ import { connection } from "../database.js"
 
 export default async function rentalList(req, res) {
     try {
-        let rentals = (await connection.query('SELECT * FROM rentals')).rows;
-        // console.log(rentals)
+        const { customerId, gameId } = req.query;
+
+        let rentals;
+        if (customerId) {
+            rentals = (await connection.query('SELECT * FROM rentals WHERE "customerId" = $1', [customerId])).rows;
+        } if (gameId) {
+            rentals = (await connection.query('SELECT * FROM rentals WHERE "gameId" = $1', [gameId])).rows;
+        } else {
+            rentals = (await connection.query('SELECT * FROM rentals')).rows;
+        }
+
 
         for (let i = 0; i < rentals.length; i++) {
-            const customer = (await connection.query('SELECT id, name FROM customers WHERE id = $1',[rentals[i].customerId])).rows[0];
-            let game = (await connection.query('SELECT id, name, "categoryId" FROM games WHERE id = $1',[rentals[i].gameId])).rows[0];
-            const categoryName = (await connection.query('SELECT name FROM categories WHERE id = $1',[game.categoryId])).rows[0];
-            console.log(customer, game, categoryName);
+            const customer = (await connection.query('SELECT id, name FROM customers WHERE id = $1', [rentals[i].customerId])).rows[0];
+            let game = (await connection.query('SELECT id, name, "categoryId" FROM games WHERE id = $1', [rentals[i].gameId])).rows[0];
+            const categoryName = (await connection.query('SELECT name FROM categories WHERE id = $1', [game.categoryId])).rows[0];
+
             game = {
                 ...game,
                 categoryName: categoryName.name
             }
             rentals[i] = {
                 ...rentals[i],
-                customer, 
+                customer,
                 game
             }
         }
