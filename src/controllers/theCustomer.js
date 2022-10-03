f5cbb0f1a9e994b3dccb06611f0dd8f55e850b9d
Gabriel Chiavegatto <chiavegattogomes@gmail.com>
2022-10-03 11:03:28 -0300

    get '/custumers/:id'

--- src/controllers/theCustomer.js
+++ src/controllers/theCustomer.js
@@ -1,6 +1,12 @@
+import { connection } from "../database.js";
+
 export default async function theCustomer(req,res){
     try{
-        
+        const {id} = req.params;
+        const customer = (await connection.query('SELECT * FROM customers WHERE id = $1',[id])).rows[0];
+        if(!customer){
+            return res.sendStatus(404);
+        }
         res.status(200).send(customer)
     }catch(error){
         console.log(error);
