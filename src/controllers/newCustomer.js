1b0011e6a261ec049179e1d2a69327e830126f07
Gabriel Chiavegatto <chiavegattogomes@gmail.com>
2022-10-03 11:51:12 -0300

    add new customer

--- src/controllers/newCustomer.js
+++ src/controllers/newCustomer.js
@@ -1,8 +1,27 @@
-export default async function newCustomer(req,res){
-    try{
-        
+import joi from 'joi';
+import { connection } from '../database.js';
+
+export default async function newCustomer(req, res) {
+    try {
+        const { name, phone, cpf, birthday } = req.body;
+
+        const schemaCustomer = joi.object({
+            name: joi.string().required(),
+            phone: joi.string().min(10).max(11),
+            cpf: joi.string().min(11).max(11),
+            birthday: joi.date().iso()
+        })
+        const validation = schemaCustomer.validate(req.body);
+        if(validation.error){
+            return res.sendStatus(400);
+        }
+        if(isNaN(cpf) || isNaN(phone)){
+            return res.sendStatus(400);
+        }
+        await connection.query('INSERT INTO customers (name, phone, cpf, birthday) VALUES ($1,$2,$3,$4)',[name, phone, cpf, birthday])
+
         res.sendStatus(201);
-    }catch(error){
+    } catch (error) {
         console.log(error);
         res.sendStatus(422);
     }
